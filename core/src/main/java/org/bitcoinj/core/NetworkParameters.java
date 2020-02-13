83749732c49b4d8ced2c3bc9c9262a8cdfbfe0d7
Mikhail Rozdin <mrozdin@yandex.ru>
2020-02-13 14:01:13 +0300

    update max coins

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -149,7 +149,7 @@ public abstract class NetworkParameters {
     /**
      * The maximum number of coins to be generated
      */
-    public static final long MAX_COINS = 21000000;
+    public static final long MAX_COINS = Long.MAX_VALUE;
 
     /**
      * The maximum money to be generated
